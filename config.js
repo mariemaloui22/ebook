b839fdf2f39d094a8fc28b1370ff5f6cb3b3e57f
mariemaloui22 <149721742+mariemaloui22@users.noreply.github.com>
2024-11-14 10:18:42 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://mariem:mariem@books-store-mern.gl59g.mongodb.net/bookscollection?retryWrites=true&w=majority&appName=Books-store-MERN'
+
+
+
+
+
